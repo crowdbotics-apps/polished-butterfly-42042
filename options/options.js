cee778cd5c20111f7f38041efe26b9ac779a6741
Crowdbotics <team@crowdbotics.com>
2023-07-10 08:28:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-butterfly-42042",
+  url: "https://polished-butterfly-42042.botics.co",
+  api: "https://polished-butterfly-42042.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
